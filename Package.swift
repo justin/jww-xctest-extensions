2ca4831f9823deed067fc5bba5ef8b0fc5fbc57b
Justin Williams <justinw@me.com>
2020-10-18 08:47:19 -0600

    Bump Swift version to 5.3

--- Package.swift
+++ Package.swift
@@ -1,14 +1,13 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version:5.3
 
 import PackageDescription
 
 let package = Package(
     name: "JWWTestExtensions",
     platforms: [
-        .iOS(.v12),
-        .tvOS(.v12),
-        .watchOS(.v5),
+        .iOS(.v13),
+        .tvOS(.v13),
+        .watchOS(.v6),
     ],
     products: [
         .library(name: "JWWTestExtensions", targets: ["JWWTestExtensions"]),
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A set of helpers and types I use for test bundles on Apple platforms.
 
 ## License
 
-Copyright 2019 Justin Williams. Licensed under the MIT license.
+Copyright 2020 Justin Williams. Licensed under the MIT license.
