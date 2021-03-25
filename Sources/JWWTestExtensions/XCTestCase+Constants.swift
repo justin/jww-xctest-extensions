565196c78a3aad6e44db66a92e8a8af982698b66
Justin Williams <justinw@me.com>
2021-03-25 09:16:38 -0600

    Add XCTestCase+Publisher extension

---
+++ Sources/JWWTestExtensions/XCTestCase+Constants.swift
@@ -0,0 +1,6 @@
+import XCTest
+
+public extension XCTestCase {
+    /// The default amount of time to wait for an expectation to time out.
+    static let defaultExpectationTimeout: TimeInterval = 2.0
+}
---
+++ Sources/JWWTestExtensions/XCTestCase+PublisherAwait.swift
@@ -0,0 +1,44 @@
+import Foundation
+import Combine
+import XCTest
+
+public extension XCTestCase {
+    /// Wrapper around waiting for a test expectation for a Publisher type.
+    ///
+    /// - Parameters:
+    ///   - publisher: The publisher you want to wait the result of.
+    ///   - timeout: The number of seconds until the expectation should timeout.
+    ///
+    /// - Returns: The output of the publisher upon success.
+    @discardableResult
+    func await<T: Publisher>(_ publisher: T,
+                             timeout: TimeInterval = XCTestCase.defaultExpectationTimeout,
+                             file: StaticString = #filePath,
+                             line: UInt = #line) throws -> T.Output {
+        var result: Result<T.Output, Error>?
+        let expectation = self.expectation(description: "Awaiting response from publisher under test.")
+
+        let cancellable = publisher.sink(
+            receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    result = .failure(error)
+                case .finished:
+                    break
+                }
+
+                expectation.fulfill()
+            },
+            receiveValue: { value in
+                result = .success(value)
+            }
+        )
+
+        waitForExpectations(timeout: timeout)
+        cancellable.cancel()
+
+        let unwrappedResult = try XCTUnwrap(result, "Awaited publisher did not generate any expected output", file: file, line: line)
+
+        return try unwrappedResult.get()
+    }
+}
