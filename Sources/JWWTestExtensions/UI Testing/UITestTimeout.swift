9c60eff21e00e63d1a8380c9613063a525beecf5
Justin Williams <justinw@me.com>
2023-04-18 10:07:22 -0600

    Add new UI testing helpers

--- Sources/JWWTestExtensions/UI Testing/UITestTimeout.swift
+++ Sources/JWWTestExtensions/UI Testing/UITestTimeout.swift
@@ -1,11 +1,15 @@
 import Foundation
 
-public struct UITestTimeout {
+public enum UITestTimeout {
     /// The default amount of time to wait for an expectation to time out.
     public static let unitTestExpectation: TimeInterval = 2.0
 
     /// The default amount of time to wait for an item to appear on screen during a UI test.
     public static let uiTestExpectation: TimeInterval = 10.0
 
-    private init() { }
+    /// The multiplier to use when determing how long to wait for a UI element to appear.
+    public static let appearMultiplier: TimeInterval = 1.0
+
+    /// The multiplier to use when determing how long to wait for a UI element to disappear.
+    public static let disappearMultiplier: TimeInterval = 1.0
 }
--- Sources/JWWTestExtensions/UI Testing/XCUITestCase.swift
+++ Sources/JWWTestExtensions/UI Testing/XCUITestCase.swift
@@ -5,7 +5,23 @@ open class XCUITestCase: XCTestCase {
     /// `XCUIApplication` instance that can be used for all UI tests.
     lazy open var app: XCUIApplication = {
         let app = XCUIApplication()
-        app.launchArguments += ["-ui-testing"]
+        app.launchArguments += ["ui-testing"]
         return app
     }()
+
+    /// Waits the amount of time you specify for the element’s exists property to become true.
+    public func waitForElementToAppear(_ element: XCUIElement, timeout: TimeInterval = 1.5 * UITestTimeout.appearMultiplier) {
+        waitForCondition(element: element, predicate: NSPredicate(format: "exists == true"), timeout: timeout)
+    }
+
+    /// Waits the amount of time you specify for the element’s exists property to become false.
+    public func waitForElementToDisappear(_ element: XCUIElement, timeout: TimeInterval = 6.5 * UITestTimeout.disappearMultiplier) {
+        waitForCondition(element: element, predicate: NSPredicate(format: "exists == false"), timeout: timeout)
+    }
+
+    
+    func waitForCondition(element: XCUIElement, predicate: NSPredicate, timeout: TimeInterval = 6.5 * UITestTimeout.appearMultiplier) {
+        let conditionalExpectation = expectation(for: predicate, evaluatedWith: element, handler: nil)
+        wait(for: [conditionalExpectation], timeout: timeout)
+    }
 }
